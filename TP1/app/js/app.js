d87fc27598d314b2af519b8ab10cdfcdbc04af2a
mathilde andre <mathilde.andre7@orange.fr>
2016-02-11 13:55:00 +0100

    addign basic login page

--- TP1/app/js/app.js
+++ TP1/app/js/app.js
@@ -9,27 +9,46 @@ angular
     .config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
         $routeProvider.when('/projects/:projectName?', {
             templateUrl: '../partials/list.html',
-            controller: 'projectsCtrl'
+            controller: 'projectsCtrl',
+            access: {isFree: false}
         })
             .when('/newProject/:projectId?', {
                 templateUrl: '../partials/form.html',
-                controller: 'newProjectController'
+                controller: 'newProjectController',
+                access: {isFree: false}
             })
             .when('/detail/:projectId?', {
                 templateUrl: '../partials/detail.html',
-                controller: 'detailController'
+                controller: 'detailController',
+                access: {isFree: false}
             })
             .when('/welcome', {
                 templateUrl: '../partials/main.html',
-                controller: 'projectsCtrl'
+                controller: 'projectsCtrl',
+                access: {isFree: true}
             })
             .when('/login', {
                 templateUrl: '../partials/login.html',
-                controller: 'loginController'
+                controller: 'loginController',
+                access: {isFree: true}
             })
             .otherwise({
                 redirectTo:'/welcome'
             });
 
         $httpProvider.interceptors.push('$httpErrorInterceptor');
-    }]);
+    }])
+
+    .run(function ($rootScope, $location) {
+
+        // Redirect to login if route requires auth and you're not logged in
+        $rootScope.$on('$routeChangeStart', function (event, next) {
+
+            if (next && !next.access.isFree && !$rootScope.currentUser) {
+                $location.path('/login');
+            }
+        });
+    });
+;
+
+
--- TP1/app/js/controllers/login-controller.js
+++ TP1/app/js/controllers/login-controller.js
@@ -1,15 +1,14 @@
 angular.module("myContollers")
-    .controller('loginController', ['$scope', 'loginFactory', function ($scope, loginFactory) {
+    .controller('loginController', ['$scope', '$rootScope','loginFactory', '$location', function ($scope,$rootScope, loginFactory, $location) {
 
 
         $scope.login = function(){
             loginFactory.login($scope.userLogin, $scope.password).then(function(user){
-                //alert("user : "+user+" is connected");
-                console.log(user.role);
-                window.user = user;
-                console.log(window.user);
-
+                console.log("User : "+user.email+" is connected")
+                $rootScope.currentUser = user;
+                $location.path("/projects");
+            }, function(err) {
+                $scope.errors = "Unable to login";
             })
         }
-
     }])
